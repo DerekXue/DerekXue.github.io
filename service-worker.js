84ff497ad784ef6829a2623f0d5384af2a21d5d9
derek.xue <derek.xue@kingmakergames.co>
2022-02-02 00:43:47 +0800

    rebuild

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.19512b1aaab973edfb27aac25dd34c0f.js"
+  "/precache-manifest.cb81c08041ef80fc7200995f4ec3db82.js"
 );
 
 self.addEventListener('message', (event) => {
